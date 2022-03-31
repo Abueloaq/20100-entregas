9460e11e14f8a249c6ec29835d860ed1b31e0efe
Abueloaq <jorgewillebald@gmail.com>
2022-03-30 21:07:42 -0300

    Segunda preentrega proyecto final

--- js/main.js
+++ js/main.js
@@ -2,26 +2,53 @@
 // Un poco de buena suerte nunca viene mal
 console.log(`Hola mundo`)
 
-const monedasAceptadas = [`USDT`, `BTC`, `LUNA`]
 
-const monedas = [
-    {id: `BTC`, name: `Bitcoin`, value: 38000},
-    {id: `LUNA`, name: `Luna`, value: 87},
-    {id: `USDT`, name: `Tether`, value: 1}
-];
 
-const wallet1 = new WALLET (`Jorge Willebald`, 13, 5, 500)
-//wallet1.comprarMoneda();
-//wallet1.transferirMoneda();
+const monedasAceptadas = [`BTC`, `LUNA`, `USDT`]
 
-//let saldoTotal = 0
+const cotizacionMonedas = [46800, 94, 1];
+
+const wallet1 = new WALLET (`Jorge Willebald`, 3, 5, 500)
+
+let elemento = document.getElementById("capa-variable");
+
+const validar = validarStoragePersonalizado();
+
+function validarStoragePersonalizado(){
+        if((localStorage.getItem("personalizado") == null) || (localStorage.getItem("personalizado") == `azul`)){
+            elemento.className = "main-layout";
+            localStorage.setItem("personalizado", `azul`)
+        }else if (localStorage.getItem("personalizado") == `verde`){
+            console.log("verde");
+            elemento.className = "main-layout-green";
+        }else {
+            elemento.className = "main-layout-red";
+        }
+    };
+
+const personalizarBilletera = function () {
+    if (elemento.className == "main-layout") {
+    elemento.className = "main-layout-green";
+    localStorage.setItem("personalizado", `verde`)
+    }else if (elemento.className == "main-layout-green"){
+    elemento.className = "main-layout-red";
+    localStorage.setItem("personalizado", `rojo`)
+    }else {
+        elemento.className = "main-layout";
+        localStorage.setItem("personalizado", `azul`);    
+    }
+}
+
+let acumuladorSaldoTotal = 0;
+
 mostrarSaldos ();
 
 function mostrarSaldos () {
     document.getElementById(`saldoBitcoinMostrado`).innerHTML = `<span>${wallet1.saldoBitcoin}</span>`;
     document.getElementById(`saldoLunaMostrado`).innerHTML = `<span>${wallet1.saldoLuna}</span>`;
     document.getElementById(`saldoUsdtMostrado`).innerHTML = `<span>${wallet1.saldoUsdt}</span>`;
-    //document.getElementById(`saldoTotalUsd`).innerHTML = `<span>${saldoTotal}</span>`;
+    acumuladorSaldoTotal = (((cotizacionMonedas[0]) * (wallet1.saldoBitcoin)) + ((cotizacionMonedas[1]) * (wallet1.saldoLuna)) + ((cotizacionMonedas[2]) * (wallet1.saldoUsdt)));
+    document.getElementById(`saldoTotalUsd`).innerHTML = `<span>${acumuladorSaldoTotal}</span>`;
 }
 
 function WALLET(usuario, saldoBitcoin = 0, saldoLuna = 0, saldoUsdt = 0) {
@@ -42,23 +69,40 @@ function WALLET(usuario, saldoBitcoin = 0, saldoLuna = 0, saldoUsdt = 0) {
         switch (currency) {
             case `BTC`:
                 this.saldoBitcoin += compraMoneda;
+                swal({
+                    title: "Saldo Actualizado",
+                    icon: "success",
+                    button: "Aceptar",
+                });
                 document.getElementById('wallet').scrollIntoView();
-                alert(`Saldo actualizado`)
+                document.getElementById('monedaAComprar').value = ``;
+                document.getElementById('cantidadAComprar').value = ``;
             break;
             case `LUNA`:
                 this.saldoLuna += compraMoneda;
+                swal({
+                    title: "Saldo Actualizado",
+                    icon: "success",
+                    button: "Aceptar",
+                });
                 document.getElementById('wallet').scrollIntoView();
-                alert(`Saldo actualizado`)
+                document.getElementById('monedaAComprar').value = ``;
+                document.getElementById('cantidadAComprar').value = ``;
             break;
             case `USDT`:
                 this.saldoUsdt += compraMoneda;
+                swal({
+                    title: "Saldo Actualizado",
+                    icon: "success",
+                    button: "Aceptar",
+                });
                 document.getElementById('wallet').scrollIntoView();
-                alert(`Saldo actualizado`)
+                document.getElementById('monedaAComprar').value = ``;
+                document.getElementById('cantidadAComprar').value = ``;
             break;
         }
         mostrarSaldos ();
         document.getElementById(`monedaAComprar`).innerHTML = ``;
-        //console.log(`El saldo de ${wallet1.usuario} es: /${wallet1.saldoBitcoin} Bitcoin /${wallet1.saldoLuna} LUNA /${wallet1.saldoUsdt} USDT`);
     }
     this.transferirMoneda = function () {
         let currency = document.getElementById(`monedaATransferir`).value.toUpperCase();
@@ -73,34 +117,63 @@ function WALLET(usuario, saldoBitcoin = 0, saldoLuna = 0, saldoUsdt = 0) {
         switch (currency) {
             case `BTC`:
                 if (this.saldoBitcoin < transfiereMoneda) {
-                    alert(`Saldo insuficiente`)
+                    swal({
+                        title: "Saldo Insuficiente",
+                        icon: "error",
+                        button: "Aceptar",
+                    });
                 } else {
                 this.saldoBitcoin -= transfiereMoneda;
+                swal({
+                    title: "Saldo Actualizado",
+                    icon: "success",
+                    button: "Aceptar",
+                });
                 document.getElementById('wallet').scrollIntoView();
-                alert(`Saldo actualizado`)
+                document.getElementById('monedaATransferir').value = ``;
+                document.getElementById('cantidadATransferir').value = ``;
                 }
             break;
             case `LUNA`:
                 if (this.saldoLuna < transfiereMoneda) {
-                    alert(`Saldo insuficiente`)
+                    swal({
+                        title: "Saldo Insuficiente",
+                        icon: "error",
+                        button: "Aceptar",
+                    })
                 } else {
                 this.saldoLuna -= transfiereMoneda;
+                swal({
+                    title: "Saldo Actualizado",
+                    icon: "success",
+                    button: "Aceptar",
+                });
                 document.getElementById('wallet').scrollIntoView();
-                alert(`Saldo actualizado`)
+                document.getElementById('monedaATransferir').value = ``;
+                document.getElementById('cantidadATransferir').value = ``;
                 }
             break;
             case `USDT`:
                 if (this.saldoUsdt < transfiereMoneda) {
-                    alert(`Saldo insuficiente`)
+                    swal({
+                        title: "Saldo Insuficiente",
+                        icon: "error",
+                        button: "Aceptar",
+                    })
                 } else {
                 this.saldoUsdt -= transfiereMoneda;
+                swal({
+                    title: "Saldo Actualizado",
+                    icon: "success",
+                    button: "Aceptar",
+                });
                 document.getElementById('wallet').scrollIntoView();
-                alert(`Saldo actualizado`)
+                document.getElementById('monedaATransferir').value = ``;
+                document.getElementById('cantidadATransferir').value = ``;
                 }
             break;
         }
         mostrarSaldos ();
-        //console.log(`El saldo de ${wallet1.usuario} es: /${wallet1.saldoBitcoin} Bitcoin /${wallet1.saldoLuna} LUNA /${wallet1.saldoUsdt} USDT`);
     }
 }
 
